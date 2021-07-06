595f087586e33e8a96ffe4668efcfe63726ed22e
shuvro-das <43287266+shuvro-das@users.noreply.github.com>
2021-07-06 12:26:35 +0530

    Update medium.js

--- server/medium.js
+++ server/medium.js
@@ -90,38 +90,3 @@ const self = {
 
 module.exports = self;
 
-// // const puppeteer = require("puppeteer");
-// // const suburl = (medium) => `https://medium.com/tag/${medium}/latest`;
-// // // const mediumDB = require("./Database/DbSchema.js");
-// // // const mongoose = require("mongoose");
-// // const self = {
-// //   browser: null,
-// //   pages: null,
-
-// //   initialize: async (medium) => {
-// //     const browser = await puppeteer.launch({ headless: false });
-// //     const page = await browser.newPage();
-
-// //     await page.goto(suburl(medium), { timeout: 0 });
-
-// //     let urls = await page.evaluate(() => {
-// //       var n = 10;
-// //       const allPosts = document.querySelectorAll(
-// //         "#root > div > div > div > div > div.l > div:nth-child(2) > div.l > div.ae"
-// //       );
-// //       const latestNPosts = [...allPosts].slice(0, n);
-// //       latestNPosts.map((el) => {
-// //         const details = {};
-// //         details.creator = el.querySelector("div > div > div h4").textContent;
-// //         details.title = el.querySelector("div > div > div h2").textContent;
-// //         details.url = el.querySelector("div > div > div:nth-child(2) > a").href;
-// //         return details;
-// //       });
-// //     });
-// //     browser.close();
-// //     console.log(details);
-// //     return resolve(urls);
-// //   },
-// // };
-
-// // module.exports = self;
